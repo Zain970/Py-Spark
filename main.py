30fff714982e6b733c494842e71775915de92955
Zain970 <itszain20@gmail.com>
2023-10-25 19:04:21 +0500

    finding the total amount spent on each word during the comopaign

--- main.py
+++ main.py
@@ -2,17 +2,32 @@ from pyspark import SparkContext
 
 
 def parser(line):
-    fields = line.split("::")
-    age = int(fields[2])
-    no_of_friends = int(fields[3])
+    fields = line.split(",")
+    sentence = fields[0]
+    amount_spent = float(fields[10])
 
-    return (age, (no_of_friends, 1))
+    return (amount_spent, sentence)
 
 
-sc = SparkContext("local[*]", "wordcount")
+sc = SparkContext("local[*]", "amount spent on each word")
 
 # Change the logging level to only display errors
 sc.setLogLevel("ERROR")
 
 # Reading the text file
-input_rdd = sc.textFile("E:/Big data course/Week-10/DataSets/big_data_compaign.csv")
+input_rdd = sc.textFile("E:/Big data course/Week-10/DataSets/big_data_campaign_data.csv")
+
+mapped_input = input_rdd.map(parser)
+
+words = mapped_input.flatMapValues(lambda x: x.split(" "))
+
+final_mapped = words.map(lambda x: (x[1].lower(), x[0]))
+
+total = final_mapped.reduceByKey(lambda x, y: x + y)
+
+sorted_result = total.sortBy(lambda x: x[1],False)
+
+result = sorted_result.take(20)
+
+for item in sorted_result.collect():
+    print("--> ", item)
